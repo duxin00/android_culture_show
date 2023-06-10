72dedcb1c27be7c10a973fca0b8ad897cb6f6576
duxin00 <91530385+duxin00@users.noreply.github.com>
2023-06-10 23:22:14 +0800

    up

---
+++ shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/culture103/EmptyFragment.kt
@@ -0,0 +1,25 @@
+package com.google.codelabs.mdc.kotlin.culture103
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+
+
+class EmptyFragment : Fragment() {
+
+    private lateinit var button: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.empty_info, container, false)
+        return view
+    }
+
+}
---
+++ shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/culture103/LoginFragment.kt
@@ -0,0 +1,67 @@
+package com.google.codelabs.mdc.kotlin.culture103
+
+import android.content.Intent
+import android.os.Bundle
+import android.text.Editable
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.shr_login_fragment.*
+import kotlinx.android.synthetic.main.shr_login_fragment.view.*
+import androidx.navigation.NavOptions
+import androidx.navigation.fragment.findNavController
+
+/**
+ * Fragment representing the login screen for Shrine.
+ */
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.shr_login_fragment, container, false)
+        // Set an error if the password is less than 8 characters.
+        view.next_button.setOnClickListener {
+            if (!isUsernameValid(username_edit_text.text)) {
+                username_text_input.error = getString(R.string.shr_error_username)
+            }
+            else if (!isPasswordValid(password_edit_text.text)) {
+                password_text_input.error = getString(R.string.shr_error_password)
+            } else {
+                password_text_input.error = null // Clear the error
+                username_text_input.error = null // Clear the error
+//                (activity as NavigationHost).navigateTo(ProductGridFragment(), false) // Navigate to the next Fragment
+                val navOptions = NavOptions.Builder()
+                    .setEnterAnim(R.animator.fade_in)
+                    .setExitAnim(R.animator.fade_out)
+                    .build()
+                (activity as NavigationHost).navigateTo(CultureListFragment(), false) // Navigate to the next Fragment
+//                val action = actionLoginFragmentToProductGridFragment()
+//                findNavController().navigate(action)
+            }
+        }
+
+        // Clear the error once more than 8 characters are typed.
+        view.password_edit_text.setOnKeyListener { _, _, _ ->
+            if (isPasswordValid(password_edit_text.text)) {
+                password_text_input.error = null //Clear the error
+            }
+            false
+        }
+        return view
+    }
+
+    /*
+        In reality, this will have more complex logic including, but not limited to, actual
+        authentication of the username and password.
+     */
+    private fun isPasswordValid(text: Editable?): Boolean {
+//        return text != null && text.length >= 6
+        return text.toString() == getString(R.string.password)
+    }
+
+    private fun isUsernameValid(text: Editable?): Boolean {
+        return text != null && text.toString() == getString(R.string.username)
+    }
+}
